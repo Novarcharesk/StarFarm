3e98d19a1ac0cf1fb44a075792f4a523dcf8f6ef
Nova Cravey <114209659+Novarcharesk@users.noreply.github.com>
2023-07-06 18:26:51 +1000

    .

--- Assets/Scripts/GrapeSpawner.cs
+++ Assets/Scripts/GrapeSpawner.cs
@@ -2,22 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum StarColor
-{
-    Red,
-    Green,
-    Blue,
-    Yellow
-}
-
 public class GrapeSpawner : MonoBehaviour
 {
     public GameObject[] fruitPrefabs; // Array of fruit prefabs in matching order of colors
     public Transform spawnPoint; // Position to spawn the fruit
 
-    public void SpawnFruit(StarColor starColor)
+    public void SpawnFruit(StarColor fruitColor) // Update the parameter type to StarColor
     {
-        int colorIndex = (int)starColor;
+        int colorIndex = (int)fruitColor; // Cast the enum to an integer index
         if (colorIndex != -1 && colorIndex < fruitPrefabs.Length)
         {
             GameObject fruitPrefab = fruitPrefabs[colorIndex];
@@ -27,14 +19,27 @@ public class GrapeSpawner : MonoBehaviour
             SpriteRenderer fruitRenderer = fruit.GetComponent<SpriteRenderer>();
             if (fruitRenderer != null)
             {
-                fruitRenderer.color = GetColorFromStarColor(starColor);
+                fruitRenderer.color = GetColorFromStarColor(fruitColor); // Use a helper method to convert the StarColor to Color
             }
 
             // Set any additional properties or logic for the spawned fruit
         }
     }
 
-    private Color GetColorFromStarColor(StarColor starColor)
+    private int GetColorIndex(StarColor color) // Update the parameter type to StarColor
+    {
+        for (int i = 0; i < fruitPrefabs.Length; i++)
+        {
+            SpriteRenderer spriteRenderer = fruitPrefabs[i].GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null && spriteRenderer.color == GetColorFromStarColor(color)) // Use the helper method to compare colors
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private Color GetColorFromStarColor(StarColor starColor) // Helper method to convert StarColor to Color
     {
         switch (starColor)
         {
