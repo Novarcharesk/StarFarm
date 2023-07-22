2a170e1642544d9a52e7a8ee8ed389d106417201
Nova Cravey <114209659+Novarcharesk@users.noreply.github.com>
2023-07-22 16:06:23 +1000

    More UI and changes to Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,31 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
 
 public class Timer : MonoBehaviour
 {
     float currentTime = 0f;
-    float startingTime = 10.4f;
+    float startingTime = 90f;
 
-
+    public TMP_Text countdownText;
+    public TMP_Text gameOverText;
     public GameObject gameOverUI;
 
-
-    public string newGameScene;
-
-
-    [SerializeField] TMP_Text countdownText;
-
+    private string currentSceneName;
 
     // Start is called before the first frame update
     void Start()
     {
         currentTime = startingTime;
+        currentSceneName = SceneManager.GetActiveScene().name;
     }
-
 
     // Update is called once per frame
     void Update()
@@ -34,46 +28,24 @@ public class Timer : MonoBehaviour
         currentTime -= Time.deltaTime;
         countdownText.text = currentTime.ToString("0");
 
-
         if (currentTime < 0)
         {
             Debug.Log("GameOver!");
             Time.timeScale = 0f;
             gameOverUI.SetActive(true);
-
-
+            gameOverText.gameObject.SetActive(true);
         }
-
 
         if (gameOverUI.activeSelf && Input.GetKeyDown(KeyCode.R))
         {
             Time.timeScale = 1f;
-            Scene scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name);
+            SceneManager.LoadScene(currentSceneName);
         }
-
 
         if (gameOverUI.activeSelf && Input.GetKeyDown(KeyCode.Space))
         {
             Time.timeScale = 1f;
-            SceneManager.LoadScene(newGameScene);
+            SceneManager.LoadScene(currentSceneName);
         }
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
